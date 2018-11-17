211f77aa2b1e2ac65f88e1fefb6360f273a7b51e
teknomunk <teknomunk@protonmail.com>
2018-11-16 21:18:00 -0600

    Start work on making Murmur::Hasher a drop-in replacement for Crystal::Hasher

--- src/murmur.cr
+++ src/murmur.cr
@@ -2,9 +2,74 @@
 module Murmur
 	VERSION = "0.1.0"
 
-	class Hasher
-		@seed : UInt32 = uninitialized UInt32
-		def initialize( @seed )
+	struct Hasher
+		@seed : UInt32
+		def initialize( @seed : UInt32 )
+		end
+
+		def result()
+			@seed
+		end
+
+		def nil()
+			return self
+		end
+		def bool( v : Bool )
+			compute( 1_u8 )
+			compute( v )
+			return self
+		end
+		def int( i )
+			compute( 2_u8 )
+			compute(i)
+			return self
+		end
+		def float( f )
+			compute( 3_u8 )
+			compute( f )
+			return self
+		end
+		def char( c )
+			compute( 4_u8 )
+			compute( c )
+			return self
+		end
+		def enum( e )
+			compute( 5_u8 )
+			compute( e.value )
+			return self
+		end
+		def symbol( s )
+			compute( 6_u8 )
+			compute( s.to_i )
+			return self
+		end
+		def reference( r )
+			compute( 7_u8 )
+			compute( r.object_id)
+			return self
+		end
+		def string( s )
+			compute( 8_u8 )
+			compute( s.to_slice ) 
+			return self
+		end
+		def class( c )
+			compute( 9_u8 )
+			compute( c.crystal_type_id )
+			return self
+		end
+		def bytes( b : Bytes )
+			compute( 10_u8 )
+			compute( b )
+			return self
+		end
+
+		private def compute( v )
+			return compute( pointerof(v).unsafe_as(Pointer(UInt8)), sizeof(typeof(v)) )
+		end
+		private def compute( slice : Slice(_) )
+			return compute( slice.to_unsafe().unsafe_as(Pointer(UInt8)), slice.size * sizeof(typeof(slice[0])) )
 		end
 
 		private def compute( key : Pointer(UInt8), len : Int32 )
